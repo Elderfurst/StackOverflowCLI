0d2fbbce854b726ad1e8771f5a78782d6ab87e28
Nick Anderson <njcanderson@gmail.com>
2017-11-22 22:07:23 -0500

    Initial commit with very basic functionality

---
+++ StackOverflowCLI/Program.cs
@@ -0,0 +1,52 @@
+﻿using RestSharp;
+using System.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace StackOverflowCLI
+{
+    public class Program
+    {
+        private static string apiKey = ConfigurationManager.AppSettings["APIKey"];
+
+        public static void Main(string[] args)
+        {
+            var answer = new Question();
+
+            var soSearch = string.Join(" ", args);
+
+            var client = new RestClient("https://api.stackexchange.com");
+            var request = new RestRequest("2.2/search/advanced");
+
+            request.AddParameter("pagesize", "1");
+            request.AddParameter("order", "desc");
+            request.AddParameter("sort", "votes");
+            request.AddParameter("site", "stackoverflow");
+            request.AddParameter("filter", "withbody");
+            request.AddParameter("accepted", "True");
+            request.AddParameter("title", soSearch);
+
+            var response = JObject.Parse(client.Execute(request).Content);
+
+            answer.Title = response["items"][0]["title"].ToString();
+            answer.Body = response["items"][0]["body"].ToString();
+
+            var answerId = (int)response["items"][0]["accepted_answer_id"];
+
+            var answerRequest = new RestRequest("2.2/answers/" + answerId);
+
+            answerRequest.AddParameter("pagesize", "1");
+            answerRequest.AddParameter("order", "desc");
+            answerRequest.AddParameter("sort", "votes");
+            answerRequest.AddParameter("site", "stackoverflow");
+            answerRequest.AddParameter("filter", "withbody");
+
+            var answerResponse = JObject.Parse(client.Execute(answerRequest).Content);
+
+            answer.AcceptedAnswer = answerResponse["items"][0]["body"].ToString();
+
+            Console.WriteLine(answer.FormatAnswer());
+        }
+    }
+}
---
+++ StackOverflowCLI/Question.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowCLI
+{
+    class Question
+    {
+        public string Title;
+        public string Body;
+        public string AcceptedAnswer;
+
+        public string FormatAnswer()
+        {
+            return "Title: " + Title + Environment.NewLine + "Body: " + Body + Environment.NewLine + "AcceptedAnswer: " + AcceptedAnswer + Environment.NewLine;
+        }
+    }
+}
