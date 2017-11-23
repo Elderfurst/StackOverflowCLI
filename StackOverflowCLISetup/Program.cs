bb1a44c21cbfc8c8572d89186982b22f2cb3f232
Nick Anderson <njcanderson@gmail.com>
2017-11-23 16:04:54 -0500

    Added setup project so that the directory housing the cli can be added to the path environment variable

---
+++ StackOverflowCLISetup/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Security.Principal;
+
+namespace StackOverflowCLISetup
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool isElevated;
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                isElevated = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            if(!isElevated)
+            {
+                Console.WriteLine("Please run this setup as an Administrator");
+                return;
+            }
+            var currentPath = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine);
+            var currentDirectory = Directory.GetCurrentDirectory();
+            if(currentPath.Contains(currentDirectory))
+            {
+                Console.WriteLine("StackOverflowCLI is already installed on this machine");
+                return;
+            }
+            Environment.SetEnvironmentVariable("Path", currentPath + currentDirectory + ";", EnvironmentVariableTarget.Machine);
+            Console.WriteLine("Successfully Installed StackOverflowCLI");
+        }
+    }
+}
